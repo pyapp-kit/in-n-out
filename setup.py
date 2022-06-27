d71c78e13f8726aa97ea31e1076d935cb1fd890b
Talley Lambert <talley.lambert@gmail.com>
2022-06-27 08:48:11 -0400

    feat: cython

--- setup.py
+++ setup.py
@@ -1,5 +1,27 @@
-# only needed until setuptools implements PEP 660
-# https://github.com/pypa/setuptools/issues/2816
+import contextlib
+import os
+
 import setuptools
 
-setuptools.setup()
+ext_modules = None
+if not os.getenv("SKIP_CYTHON"):
+    with contextlib.suppress(ImportError):
+        from Cython.Build import cythonize
+
+        # For cython test coverage install with `make build-trace`
+        compiler_directives = {"linetrace": os.getenv("CYTHON_TRACE", False)}
+
+        # Set CFLAG to all optimizations (-O3)
+        # Any additional CFLAGS will be appended.
+        # Only the last optimization flag will have effect
+        os.environ["CFLAGS"] = "-O3 " + os.environ.get("CFLAGS", "")
+
+        ext_modules = cythonize(
+            "src/in_n_out/*.py",
+            exclude=["**/__init__.py"],
+            nthreads=int(os.getenv("CYTHON_NTHREADS", 0)),
+            language_level=3,
+            compiler_directives=compiler_directives,
+        )
+
+setuptools.setup(ext_modules=ext_modules)
