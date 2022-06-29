58de2e9aacb7cf4d7db5c0f5e912030270fb143a
Talley Lambert <talley.lambert@gmail.com>
2022-06-29 18:54:57 -0400

    test: add clear tests

--- tests/test_store.py
+++ tests/test_store.py
@@ -1,6 +1,8 @@
+from typing import Optional
+
 import pytest
 
-from in_n_out import Store
+from in_n_out import Store, set_processors, set_providers
 from in_n_out._store import _GLOBAL
 
 
@@ -33,3 +35,23 @@ def test_create_get_destroy():
         Store.create(_GLOBAL)
 
     assert len(Store._instances) == 1
+
+
+def test_store_clear():
+
+    test_store = Store.create("test")
+    assert not test_store.providers
+    assert not test_store.opt_providers
+    assert not test_store.processors
+
+    set_providers({int: 1}, store=test_store)
+    set_providers({Optional[str]: None}, store=test_store)
+    set_processors({int: print}, store=test_store)
+    assert len(test_store.providers) == 1
+    assert len(test_store.opt_providers) == 1
+    assert len(test_store.processors) == 1
+
+    test_store.clear()
+    assert not test_store.providers
+    assert not test_store.opt_providers
+    assert not test_store.processors
